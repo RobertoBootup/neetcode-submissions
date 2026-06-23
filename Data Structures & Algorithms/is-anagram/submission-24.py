cdd3db0a06a345f690f2cde99bcbb29f697a68c7
Roberto <robertofenway@gmail.com>
2026-06-23 19:33:42 -0400

    Add: is-anagram - submission-24

---
+++ Data Structures & Algorithms/is-anagram/submission-24.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s)==sorted(t)
+        
+
+        
+        
+
+        
+        
+
+
+
+
+
+       
+         
+
+            
+            
